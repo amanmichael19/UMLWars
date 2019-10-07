55eb9548b6eb4c0ea61f4aef3f40c9f560d9bf51
CJenkins <jenki235@msu.edu>
2019-10-07 09:00:03 -0400

    Create Hit Test. Adds a hit test to the Game which cycles through and calls the hit test of each game object. Changed constructor of GameObject to facilitate

--- project1/GameObject.h
+++ project1/GameObject.h
@@ -9,11 +9,30 @@
  */
 
 #pragma once
+
+
+class CGame;
+
+/**
+ * GameObject class to hold various game objects
+ */
+
 class CGameObject
 {
 public:
 
-	CGameObject();
+	/// Creates all objects in the game
+	CGameObject(CGame* game);
+
+public:
+
+	/// Default constructor (disabled)
+	CGameObject() = delete;
+
+	/// Copy constructor (disabled)
+	CGameObject(const CGameObject&) = delete;
+
+	
 
 
 	/** The X location of the item
@@ -26,16 +45,40 @@ public:
 	 */
 	double GetY() const { return mY; }
 
-	/// Set the item location
+	/// Set the object location
 	/// \param x X location
 	/// \param y Y location
 	virtual void SetLocation(double x, double y) { mX = x; mY = y; }
 
+	/// Set the object dimensions
+	/// \param x width
+	/// \param y height
+	virtual void SetDimensions(double wid, double hit) { mWidth = wid; mHeight = hit; }
+
+	/** The Width of the item
+	 * \returns double of Width 
+	 */
+	double GetWidth() const { return mWidth; }
+
+	/** The height of the item
+	 * \returns double of height
+	 */
+	double GetHeight() const { return mHeight; }
+
+	bool HitTest(int x, int y);
+
 
 private:
 
-	//GameObject location 
+	//GameObject location and center of object
 	double mX = 0; ///< X location of object
 	double mY = 0; ///< Y location of object
+
+	//GameObject dimensions
+	double mWidth = 0; ///< Width of object
+	double mHeight = 0; ///< Height of object
+
+	/// The Game for this object
+	CGame* mGame;
 };
 
