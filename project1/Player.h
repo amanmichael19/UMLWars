1f7d20fa1fedb4fe3171f8549cca52820ce1c83e
Funakoshi De Silva <silvade1@egr.msu.edu>
2019-10-08 03:56:06 -0400

    Implementation of Player

--- project1/Player.h
+++ project1/Player.h
@@ -1,18 +1,55 @@
 /**
  * \file Player.h
  *
- * \author Amanuel
+ * \author Funakoshi Silva
  *
- * 
+ *
  */
 
 
 #pragma once
 #include "GameObject.h"
-/**
- * class implementing player
- */
+#include <memory>
+#include <string>
+#include <vector>
+
+
+
+ /**
+  * class implementing player
+  */
 class CPlayer : public CGameObject
 {
+public:
+	/// Default constructor
+	CPlayer(CGame* game);
+
+	void OnDraw(Gdiplus::Graphics* graphics);
+
+	/// Default constructor (disabled)
+	CPlayer() = delete;
+
+	//virtual void Draw(Gdiplus::Graphics* graphics);
+
+private:
+	std::unique_ptr<Gdiplus::Bitmap> mPlayerImage;
+
+	// All of the items to populate our aquarium
+	std::vector<std::shared_ptr<CGameObject> > mItems;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
