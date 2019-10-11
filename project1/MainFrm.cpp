38831ddb9a0513c424132f00559341c240cb0404
Amanuel Tesfazgi Tesfamichael <tesfami1@msu.edu>
2019-10-11 15:03:18 -0400

    change size to match size of scoreboard

--- project1/MainFrm.cpp
+++ project1/MainFrm.cpp
@@ -82,8 +82,8 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
 	// TODO: Modify the Window class or styles here by modifying
 	//  the CREATESTRUCT cs
 
-	cs.cx = 780;
-    cs.cy = 710;
+	cs.cx = 1250;
+    cs.cy = 1000;
 
 	cs.dwExStyle &= ~WS_EX_CLIENTEDGE;
 	cs.lpszClass = AfxRegisterWndClass(0);
