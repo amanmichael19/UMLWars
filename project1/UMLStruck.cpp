84b16608772cb1621997ffc24723f8a72b84e9fe
Ziyuan Zhang <zhang835@msu.edu>
2019-10-21 13:38:17 -0400

    message display update: green for good, red for unfair

--- project1/UMLStruck.cpp
+++ project1/UMLStruck.cpp
@@ -15,14 +15,17 @@ using namespace std;
 void CUMLStruck::Draw(Gdiplus::Graphics* graphics)
 {
 	// Colors to be used
-	SolidBrush yellowBrush(Color(255, 0, 0));
-	//SolidBrush blackBrush(Color(0, 0, 0));
-	Pen blackPen(Color(0, 0, 0));
+	SolidBrush redwBrush(Color(255, 0, 0));
+	SolidBrush greenBrush(Color(34, 139, 34));
 
 	// Font to be used
 	FontFamily fontFamily(L"Arial");
-	Gdiplus::Font font(&fontFamily, 18);
-
-	graphics->DrawString(mMsg.c_str(), -1, &font, PointF(GetX(), GetY()), &yellowBrush);
+	Gdiplus::Font font(&fontFamily, 20);
+	if (mMsg == L"Unfair") {
+		graphics->DrawString(mMsg.c_str(), -1, &font, PointF(GetX(), GetY()), &redwBrush);
+	}
+	else {
+		graphics->DrawString(mMsg.c_str(), -1, &font, PointF(GetX(), GetY()), &greenBrush);
+	}
 	
 }
